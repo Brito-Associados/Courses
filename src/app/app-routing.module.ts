520822be3e6459fc0890187a9e4c478ea4a018ca
jersonb <jersonlbrito@hotmail.com>
2021-10-12 22:18:13 -0300

    iniciando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
+  { path: 'course', loadChildren: () => import('./pages/course/course.module').then(m => m.CourseModule) },
+  { path: 'financial', loadChildren: () => import('./pages/financial/financial.module').then(m => m.FinancialModule) },
+  { path: 'class', loadChildren: () => import('./pages/class/class.module').then(m => m.ClassModule) },
+  { path: 'teacher', loadChildren: () => import('./pages/teacher/teacher.module').then(m => m.TeacherModule) },
+  { path: 'student', loadChildren: () => import('./pages/student/student.module').then(m => m.StudentModule) },
+  { path: 'user', loadChildren: () => import('./pages/user/user.module').then(m => m.UserModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
